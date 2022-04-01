27c773dd1b3b086475cc425502dd376f7c551ea1
OnceUponAtime2z <94980294+OnceUponAtime2z@users.noreply.github.com>
2022-04-02 03:33:07 +0700

    prepare to deploy

--- webapp/local_settings.py
+++ webapp/local_settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -90,8 +88,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
