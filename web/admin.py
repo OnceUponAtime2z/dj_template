11d33fa3c4df5bf10c1d83462e1e795c2b0d5158
OnceUponAtime2z <94980294+OnceUponAtime2z@users.noreply.github.com>
2022-04-01 22:15:34 +0700

    update config

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from web.models import Blog, Author
 
 # Register your models here.
+# admin.site.register(Blog)
+# admin.site.register(Author)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'date_created']
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['prefix', 'first_name', 'last_name', 'dateofbirth']
+
+    
+
+    
--- web/models.py
+++ web/models.py
@@ -1,3 +1,56 @@
+from distutils.sysconfig import PREFIX
+from sre_constants import CATEGORY
+from sys import prefix
+from unicodedata import category
 from django.db import models
 
+CATEGORY_CHOICES = [
+    (1, 'News'),
+    (2, 'Advertisement'),
+    (3, 'Memo'),
+]
+
+PREFIX_CHOICES = [
+    (1, 'Mr'),
+    (2, 'Miss'),
+    (3, 'Mrs'),
+]
+
 # Create your models here.
+class Blog(models.Model):
+
+    title = models.CharField(max_length=255)
+    category = models.IntegerField(choices=CATEGORY_CHOICES, default=1)
+    content = models.TextField()
+    date_updated = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        return self.title
+
+
+
+    def get_absolute_url(self):
+        return reverse("Blog_detail", kwargs={"pk": self.pk})
+
+class Author(models.Model):
+
+    prefix = models.IntegerField(choices=PREFIX_CHOICES)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dateofbirth = models.DateField()
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+    def __str__(self):
+        return self.first_name +" "+self.last_name
+
+    def get_absolute_url(self):
+        return reverse("Author_detail", kwargs={"pk": self.pk})
+
